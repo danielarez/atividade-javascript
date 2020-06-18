99d14995d7fc2c8d4b707fd379d64e3cf641fe80
daniela rezende <danyrezende96@gmail.com>
2020-06-18 00:46:42 -0300

    nova atividade

---
+++ meuScript.js
@@ -0,0 +1,16 @@
+
+
+          let num1 = window.prompt("digite um número")
+
+          let num2 = window.prompt("digite outro número")
+          
+          if (num1 < num2) {
+           
+            alert(num2)
+          }
+          
+          else  { 
+
+           alert (num1)
+          
+          }
